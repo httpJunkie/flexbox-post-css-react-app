caa3d6f12acb1216c7a4d9d31065709d252d17a6
Eric Bishard <e.bishard@gmail.com>
2018-07-02 07:53:40 -0700

    Add new utility classes for padding, add font-awesome icon in header. WIP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 
 class Header extends Component {
   render() {
     return (
       <header className="flex-item flex-container col">
         <div className="flex-item flex-container height-100">
-          <div className="flex-item flex-container horiz-center vert-center">
+          <div className="flex-item flex-container horiz-start vert-center">
             <div className="width-100">
-            <span className="title">PORTFOLIO</span>
+              <span className="title">PORTFOLIO</span>
             </div>
           </div>
-          <div className="flex-item flex-container horiz-center vert-center">
-            <div className="text-center width-80">right</div>
+          <div className="flex-item flex-container vert-center">
+            <div className="text-right width-100 horiz-end padding-right-1">
+              <FontAwesome name='twitter-square' />
+            </div>
           </div>
         </div>
       </header>
